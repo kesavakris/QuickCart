013bcf4bb4d270694f22c706ca073f65f90fc5c4
kesava krishna <142408246+kesavakris@users.noreply.github.com>
2025-04-21 11:15:24 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,22 +1,12 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
+ import { authMiddleware } from "@clerk/nextjs";
 
-// export default authMiddleware();
+ export default authMiddleware();
 
-// export const config = {
-//   matcher: [
-//     "/((?!_next|.*\\..*).*)", // Exclude static files and internal paths
-//     "/(api|trpc)(.*)",
-//   ],
-// };
+ export const config = {
+   matcher: [
+     "/((?!_next|.*\\..*).*)", // Exclude static files and internal paths
+    "/(api|trpc)(.*)",
+   ],
+ };
 
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  return response;
-}
-
-export const config = {
-  matcher: ['/api/:path*'],
-};
